a949f2607598c3c891d284728a035a16a664df6a
Jared Forsyth <jared@jaredforsyth.com>
2016-08-09 10:02:50 -0600

    show qr code!

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const app = require('express')();
 const ip = require('my-local-ip')();
 const fs = require('fs');
+const qrcode = require('qrcode');
 
 let links = [];
 try {
@@ -14,41 +15,53 @@ const save = () => {
   fs.writeFileSync('./links.json', JSON.stringify(links, null, 2));
 }
 
-app.get('/', (req, res) => {
-  if (req.query.link) {
-    links.push(req.query.link.replace('//localhost:', '//' + ip + ':'));
-    save()
-    return res.redirect('/')
-  }
-  if (req.query.nolink) {
-    links = links.filter(link => link !== req.query.nolink)
-    save()
-    return res.redirect('/')
-  }
-  res.end(
-    '<html><meta name="viewport" ' +
+const short = text => {
+  if (text.length < 100) return text
+  return text.slice(0, 50) + '..' + text.slice(-50);
+}
+
+const render = (links, qrcode) => {
+  return ('<html><meta name="viewport" ' +
     'content="width=device-width, initial-scale=1"><body>Link me ' +
     '<form method="GET"><input autofocus name="link">' +
     '<button>Submit</button></form><br/>' +
     links.map((link) => {
       return `
         <a href="?nolink=${encodeURIComponent(link)}">&times;</a>&nbsp;
-        <a target="_blank" href="${link}">${link}</a>
+        <a target="_blank" href="${link}">${short(link)}</a>
         <br/>
       `;
     }).join('\n') +
-    '<br><br><br><a href="/reset">Clear all</a></body>'
-  )
-});
+    '<br><br><br><a href="/reset">Clear all</a>' +
+    '<img src="' + qrcode + '"/>' +
+    '</body>')
+}
 
-app.get('/reset', (req, res) => {
-  links = [];
-  save();
-  res.redirect('/')
-});
+qrcode.toDataURL('http://' + ip + ':4021', (err, url) => {
+  app.get('/', (req, res) => {
+    if (req.query.link) {
+      links.push(req.query.link.replace('//localhost:', '//' + ip + ':'));
+      save()
+      return res.redirect('/')
+    }
+    if (req.query.nolink) {
+      links = links.filter(link => link !== req.query.nolink)
+      save()
+      return res.redirect('/')
+    }
+    res.end(
+      render(links, url)
+    )
+  });
 
-app.listen(4021, () => {
-  console.log("Ready on http://localhost:4021")
+  app.get('/reset', (req, res) => {
+    links = [];
+    save();
+    res.redirect('/')
+  });
+
+  app.listen(4021, () => {
+    console.log("Ready on http://localhost:4021")
+  })
 })
-
 
